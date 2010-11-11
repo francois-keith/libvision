98776a8ede6c0c4003a3d2f5bfe3dde5b99b0a67
Druon Sebastien <seb@homedruon.com>
2010-11-11 11:51:56 +0900

    added get_num_cameras to vision_server virtual

--- include/visionlib/visionprocess.h
+++ include/visionlib/visionprocess.h
@@ -16,9 +16,11 @@ class VisionServer  ;
 class VisionServer {
 	public:
 		virtual VisionProcess* get_process_by_name (string) = 0 ;			// retrieves a pointer to one of the processes run by the server
-		virtual CalibratedCamera<unsigned char>* get_camera ( int i )  = 0 ;				// retrieves a pointer to the camera number ith 
+		virtual CalibratedCamera<unsigned char>* get_camera ( int i )  = 0 ;		// retrieves a pointer to the camera number ith
+		virtual int get_num_cameras() = 0 ;						// retrieves the number of cameras managed by the server
+
 		virtual void register_to_cam ( VisionProcess*, int cam, int ringbuffer ) = 0 ;  // registers a process to a cam
-		virtual void stop() = 0 ;							// stops the server
+		virtual void stop() = 0 ;							// stops the server main loop
 };
 
 class VisionProcess {
