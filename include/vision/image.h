8e66ae96ea942fccdfb2433f6fa388c2c09dacca
Sebastien Druon <seb@homedruon.com>
2011-05-10 13:58:54 +0200

    Upgraded Image type

--- include/vision/image.h
+++ include/vision/image.h
@@ -1,120 +1,133 @@
 #ifndef VISIONLIB_IMAGE_H
 #define VISIONLIB_IMAGE_H
 
-#include <stdlib.h>
 #include <cstring>
 #include <iostream>
+
 #include <vision/imageref.h>
 
 namespace vision {
+
+
+// *************************************************
+// ***
+// *** Class: Image<Pix>
+// ***
+// *************************************************
+
+
+// This Image class is inspired from the libCVD
+//
+// The template parameter is the pixel type, so you can use
+// it with any kind of data ... 
+//
+// All members ( width, height, etc ) are declared public ... which is
+// probably not a good idea, but makes things so easier when using it !
+// Read them, use them, but don't modify them in your code, or you'll 
+// segfault ! You've been warned :)
+
 
 template < typename Pix > class Image
 {
 
 public:
 
-  Image ( ImageRef imgref ) ;
-  Image ( int width, int height);
+  unsigned int width ;		// width of the image  ( in pixels )
+  unsigned int height ;		// height of the image ( in pixels )
+  unsigned int pixels ;		// total number of pixels ( width * height ) 
+  
+  ImageRef size ;	// size of the image ( width, height )
+
+  size_t pixel_size ;   // size of a pixel in bytes
+  size_t data_size ;    // size of the image in bytes ( width*height*pixel_size )
+
+  Pix* raw_data ;	// Image data, in ROW order.
+
+public:
+
+  Image ( ImageRef size ) ;
+  Image ( const Image<Pix> &img ) ;
+  Image ( unsigned int width, unsigned int height) ;
+  
   ~Image ();
 
-  unsigned int get_width ()
-  {
-    return width;
-  }
-  unsigned int get_height ()
-  {
-    return height;
-  }
-  ImageRef get_size ()
-  {
-    return ImageRef (width, height);
-  }
 
-  Pix* get_raw_data()
-  {
-   return data ;
-  }
-
-  Pix& operator[] ( ImageRef coord )
-  {
-        return data[coord.x + coord.y * width] ;
-  }
-
-  Pix* scale(unsigned int scale_factor) ;
+  Pix& operator[] ( ImageRef coord ) ;
+  Pix* operator[] ( unsigned int line ) ;
 
   Image<Pix>* clone() ;
-
-private:
-
-  int width;
-  int height;
-  int num_pixels;	// width * height
-  Pix *data;
-
 
 };
 
 
-
-template<typename Pix> 
-Image<Pix>::Image (int w, int h)
+template<typename Pix>
+Image<Pix>::Image( ImageRef ir ) 
 {
-  width = w;
-  height = h;
-  num_pixels = w * h;
-  data = new Pix[num_pixels];
-  if (data == NULL)
+  width = ir.x;
+  height = ir.y;
+  pixels = ir.x*ir.y ;
+  size = ImageRef(ir.x,ir.y) ;
+  pixel_size = sizeof ( Pix ) ;
+  data_size = pixels * pixel_size ;
+  raw_data = new Pix[pixels];
+  if (raw_data == NULL)
     throw "new[] error" ;
-
 }
 
 template<typename Pix>
-Image<Pix>::Image( ImageRef ir ) {
-	 width = ir.x;
-	 height = ir.y;
-	 num_pixels = ir.x * ir.y;
-         data = new Pix[num_pixels];
-	 if (data == NULL)
-		throw "new[] error" ;
+Image<Pix>::Image( const Image<Pix> &img ) 
+{
+  width = img.width ;
+  height = img.height ;
+  pixels = img.pixels ;
+  size = img.size ;
+  pixel_size = img.pixel_size ;
+  data_size = img.data_size ;
+  raw_data = new Pix[pixels];
+  if (raw_data == NULL)
+    throw "new[] error" ;
+  std::memcpy ( img.raw_data, raw_data, data_size ) ;
 }
 
-template < typename Pix >
+template<typename Pix> 
+Image<Pix>::Image (unsigned int w, unsigned int h)
+{
+  width = w;
+  height = h;
+  pixels = w * h;
+  size = ImageRef(w,h) ;
+  pixel_size = sizeof ( Pix ) ;
+  data_size = pixels * pixel_size ;
+  raw_data = new Pix[pixels];
+  if (raw_data == NULL)
+    throw "new[] error" ;
+}
+
+template<typename Pix>
 Image<Pix>::~Image ()
 {
-  delete[] data;
+	delete[] raw_data;
 }
 
-template< typename Pix >							
-Pix* Image<Pix>::scale(unsigned int scale_factor) {
-    if(width % scale_factor != 0 || height % scale_factor != 0) { return 0; }
-	Pix* buff ;
-    buff = new Pix[num_pixels / (scale_factor*scale_factor) * sizeof(Pix)];
-	for (int i=0; i<width; i+=scale_factor ) {
-	for (int j=0; j<height; j+=scale_factor ) {
-		int acc =0 ;
-		for (int k=0; k<scale_factor; k++ ) { 
-		for (int l=0; l<scale_factor; l++ ) {
-			acc += (int) data[i+k+(j+l)*width] ;
-        }
-        }	
-
-		acc /= (scale_factor*scale_factor) ;
-
-		buff[i/scale_factor+width/scale_factor * j/scale_factor] = (Pix) acc ;
-	}
-    }
-
-	return buff ;
+template <typename Pix>
+Pix& Image<Pix>::operator[] ( ImageRef coord )
+{
+	return raw_data[coord.x+coord.y*width] ;
 }
 
+template <typename Pix>
+Pix* Image<Pix>::operator[] ( unsigned int line ) {
+	return &(raw_data[line*width] ) ;
+}
 
 template<typename Pix >
 Image<Pix>* Image<Pix>::clone() {
 	Image<Pix>* img ;
 	img = new Image<Pix>(width,height) ;
-	std::memcpy ( img->get_raw_data(), data, num_pixels*sizeof(Pix) ) ;
+	std::memcpy ( img->raw_data, raw_data, data_size ) ;
 	return img ;
 }
+
 
 }
 
--- src/image_fast.cpp
+++ src/image_fast.cpp
@@ -8,7 +8,7 @@ void fast9_detect ( Image<unsigned char>* img , double b, vector<ImageRef> *ret 
 
 	xy* result ;
 	int nb ;
-	result = fast9_detect_nonmax ( (const byte*) img->get_raw_data(), img->get_width(), img->get_height(), img->get_width(), b, &nb ) ; 
+	result = fast9_detect_nonmax ( (const byte*) img->raw_data, img->width, img->height, img->width, b, &nb ) ; 
 	for (int i=0; i<nb; i++)
 		ret->push_back ( ImageRef(result[i].x, result[i].y) ) ;
 	free (result) ;
