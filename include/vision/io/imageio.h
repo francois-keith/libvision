acedaa441eb7e9f8f019a759b4eb9bcee5981e2c
Sebastien Druon <seb@homedruon.com>
2011-05-12 16:53:09 +0200

    EOD

--- include/vision/io/imageio.h
+++ include/vision/io/imageio.h
@@ -1,7 +1,43 @@
 #ifndef VL_IMAGEIO_H
 #define VL_IMAGEIO_H
 
+#include <vision/vision.h>
+
+#if Vision_ImageMagick
+#include <Magick++.h>
+#endif
+
+namespace vision {
+	
+	#if Vision_IMAGEMAGICK
+
+	template<typename Pixel> 
+	void save_mono ( std::string filename, Image<Pixel>* img ) {
+		
+	}
+	
+	template<typename Pixel> 
+	void load_mono ( std::string filename, Image<Pixel>* img ) {
+
+	}
+
+	template<typename Pixel>
+	void save_rgb ( std::string filename, Image<Pixel>* img ) {
+
+	}
+
+	template<typename Pixel>
+	void load_rgb ( std::string filename, Image<Pixel>* img ) {
+
+	}
 
 
+
+	#endif
+
+
+
+
+}
 
 #endif
