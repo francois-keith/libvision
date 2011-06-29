ff9309c8899a6eeecfcd64de5c5e707c3b9d5821
Pierre Gergondet <pierre.gergondet@gmail.com>
2011-06-29 18:04:09 +0900

    Fix warning

--- src/scale.cpp
+++ src/scale.cpp
@@ -7,11 +7,11 @@ Image<unsigned char, MONO>* scale( Image<unsigned char,MONO>* src, unsigned int 
 	Image<unsigned char,MONO>* img ;
 	img = new Image<unsigned char,MONO>(src->width/scale_factor,src->height/scale_factor) ;
 	unsigned int nb = scale_factor * scale_factor ;
-	for (int i=0; i<img->width; i++ ) 
-	for (int j=0; j<img->height; j++ ) {
+	for (unsigned int i=0; i<img->width; i++ ) 
+	for (unsigned int j=0; j<img->height; j++ ) {
 		int acc =0 ;
-		for (int k=i*scale_factor; k<(i+1)*scale_factor; k++ ) 
-		for (int l=j*scale_factor; l<(j+1)*scale_factor; l++ ) {
+		for (unsigned int k=i*scale_factor; k<(i+1)*scale_factor; k++ ) 
+		for (unsigned int l=j*scale_factor; l<(j+1)*scale_factor; l++ ) {
 			acc += src->raw_data[k+l*src->width] ;
 		}
 		acc /= nb ;
