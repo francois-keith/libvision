352d7a103e886c1de3af4cff90c5bcb60e723c4c
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2011-09-28 13:23:16 +0900

    * Add match test to cmake build * Performance test * Out of range bug fix

--- test/match.cpp
+++ test/match.cpp
@@ -5,9 +5,39 @@
 #include <sstream>
 #include <ctime>
 
+#include <sys/time.h>
+
 #define SEARCH_SIZE 30 
 #define WIN_SIZE 5
 
+inline void time_diff(const timeval & tv_in, const timeval & tv_out, timeval & tv_diff)
+{
+    if( tv_out.tv_sec < tv_in.tv_sec || (tv_out.tv_sec == tv_in.tv_sec && tv_out.tv_usec < tv_in.tv_usec) )
+    {
+        time_diff(tv_out, tv_in, tv_diff);
+    }
+    else
+    {
+        if(tv_out.tv_usec < tv_in.tv_usec)
+        {
+            tv_diff.tv_sec = tv_out.tv_sec - tv_in.tv_sec - 1;
+            tv_diff.tv_usec = 1000000 - tv_in.tv_usec + tv_out.tv_usec;
+        }
+        else
+        {
+            tv_diff.tv_sec = tv_out.tv_sec - tv_in.tv_sec;
+            tv_diff.tv_usec = tv_out.tv_usec - tv_in.tv_usec;
+        }
+    }
+}
+
+inline void change_pixel(vision::Image<uint32_t, RGB> & img, int y, int x, uint32_t value)
+{
+    if(x >= 0 && x < img.width && y >= 0 && y < img.height)
+    {
+        img[y][x] = value;
+    }
+}
 
 /* The aim of this test is to test the correspondance between two images */
 
@@ -37,8 +67,8 @@ int main(int argc, char * argv[])
         return 1;
     }
 
-    unsigned int x0 = 100 ;
-    unsigned int y0 = 200 ;
+    unsigned int x0 = 360 ;
+    unsigned int y0 = 405 ;
 
     unsigned int Rx = x0 ;
     unsigned int Ry = y0 ;
@@ -62,9 +92,22 @@ int main(int argc, char * argv[])
     
     double result ;
     result = 1e16 ; 
+
+    timeval tv_in;
+    timeval tv_out;
+    timeval tv_diff;
+    gettimeofday(&tv_in, 0);
    
-    for ( int ii = x0 - SEARCH_SIZE; ii <= x0 + SEARCH_SIZE; ii++ )
-    for ( int jj = y0 - SEARCH_SIZE; jj <= y0 + SEARCH_SIZE; jj++ ) {
+    int xstart = x0 - SEARCH_SIZE;
+    xstart = xstart<0?0:xstart;
+    int ystart = y0 - SEARCH_SIZE;
+    ystart = ystart<0?0:ystart;
+    int xend = x0 + SEARCH_SIZE;
+    xend = xend>img_left->width?img_left->width:xend;
+    int yend = y0 + SEARCH_SIZE;
+    yend = yend>img_left->height?img_left->height:yend;
+    for ( int ii = xstart; ii <= xend; ii++ )
+    for ( int jj = ystart; jj <= yend; jj++ ) {
 
 	double score = 0 ;
 
@@ -99,15 +142,19 @@ int main(int argc, char * argv[])
 
 
     }
+
+    gettimeofday(&tv_out, 0);
+    time_diff(tv_in, tv_out, tv_diff);
+    std::cout << "Match took: " << (tv_diff.tv_sec + ((float)tv_diff.tv_usec/1000000)) << "s" << std::endl;
     
     // *** Dessin du cadre de recherche
 
     for ( int i = - SEARCH_SIZE ; i <= SEARCH_SIZE ; i ++ ) {
 
-    	(*img_right)[y0 + i][x0 + SEARCH_SIZE ] = 0x0000FF ;
-    	(*img_right)[y0 + i][x0 - SEARCH_SIZE ] = 0x0000FF ;
-    	(*img_right)[y0 - SEARCH_SIZE][x0 + i ] = 0x0000FF ;
-    	(*img_right)[y0 + SEARCH_SIZE][x0 + i ] = 0x0000FF ;
+    	change_pixel(*img_right,y0 + i,x0 + SEARCH_SIZE, 0x0000FF) ;
+    	change_pixel(*img_right,y0 + i,x0 - SEARCH_SIZE, 0x0000FF) ;
+    	change_pixel(*img_right,y0 - SEARCH_SIZE,x0 + i, 0x0000FF) ;
+    	change_pixel(*img_right,y0 + SEARCH_SIZE,x0 + i, 0x0000FF) ;
     }
     
     // *** Resultat de la recherche
