271a24e46cf885c8d9ed10e77304e98f0b7ae41f
Keith Francois <francois.keith@lirmm.fr>
2013-06-18 10:05:24 +0200

    Warning removal

--- include/vision/image/imageref.h
+++ include/vision/image/imageref.h
@@ -48,7 +48,7 @@ private:
   
   friend class boost::serialization::access;
   template<class Archive>
-  void serialize(Archive & ar, const unsigned int version)
+  void serialize(Archive & ar, const unsigned int /*version*/)
   {
     ar & x;
     ar & y;
